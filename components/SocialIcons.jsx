e3547dc04ce0d80be6864ade1674b22af20261f7
kiranballa126 <134221213+kiranballa126@users.noreply.github.com>
2024-07-10 19:46:47 +0530

    Update SocialIcons.jsx

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -7,15 +7,15 @@ export const SocialIcons = () => {
   return (
     <div className="md:left-10 md:bottom-0 md:fixed z-10 sm:hidden hidden md:block">
       <ul className="listNone  text-lightSlate text-xl md:flex md:justify-center md:flex-col md:items-center">
-        {/* <li className="md:py-6 md:hover:text-secondary md:hover:-translate-y-0.5 md:transition ">
+        <li className="md:py-6 md:hover:text-secondary md:hover:-translate-y-0.5 md:transition ">
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://github.com/renukajalluri"
+            href="https://github.com/kiranballa126"
           >
             <FiGithub />
           </a>
-        </li> */}
+        </li>
         <li className="md:py-6 md:hover:text-secondary md:hover:-translate-y-0.5 md:transition">
           <a
             target="_blank"
